0db9cea8f9acf0cbcafd260623c00a88e87d3f61
Jacopo Manetti <manetti.jacopo@hotmail.com>
2024-11-25 21:55:02 +0100

    fix del random e logica terzo pacchetto

--- Main.java
+++ Main.java
@@ -5,54 +5,61 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        // Inizializziamo un array di interi
         Integer[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        double vStrategia1=0, vStrategia2=0, vAtteso=5.5, vDiControllo=0;
-        Random random=new Random();
-        int a=10000000;
-        for(int i=0; i<a; i++){
-            List<Integer> list = Arrays.asList(array);
+        double vStrategia1 = 0, vStrategia2 = 0, vAtteso = 5.5, vDiControllo = 0;
+        Random random = new Random();
+        int a = 10000000;
 
+        for (int i = 0; i < a; i++) {
             // Mescoliamo l'array
+            List<Integer> list = Arrays.asList(array);
             Collections.shuffle(list);
-    
-            // Convertiamo di nuovo la lista in un array
-            list.toArray(array);
-            int nPrimoPacchetto=random.nextInt(10);
-            boolean b=true;
-            int nSecondoPaccheto=0;
-            vDiControllo+=array[nPrimoPacchetto];
-            while (b) {
-                nSecondoPaccheto=random.nextInt(10);
-                if (nSecondoPaccheto!=nPrimoPacchetto) {
-                    b=false;
-                }
-            }
-            if(array[nSecondoPaccheto]>5){
-                vStrategia1+=array[nPrimoPacchetto];
-                int nTerzoPacchetto=random.nextInt(10);
-                while ((nPrimoPacchetto!=nTerzoPacchetto)&&(nSecondoPaccheto!=nTerzoPacchetto)) {
-                    nTerzoPacchetto=random.nextInt(10);
-                }
-                vStrategia2+=array[nTerzoPacchetto];
-            }else{
-                vStrategia2+=array[nPrimoPacchetto];
-                int nTerzoPacchetto=random.nextInt(10);
-                while ((nPrimoPacchetto!=nTerzoPacchetto)&&(nSecondoPaccheto!=nTerzoPacchetto)) {
-                    nTerzoPacchetto=random.nextInt(10);
-                }
-                vStrategia1+=array[nTerzoPacchetto];
-                
-            }
-            
-       
+            Integer[] shuffledArray = list.toArray(new Integer[0]);
 
-        
+            // Selezione del primo pacchetto
+            int nPrimoPacchetto = random.nextInt(10);
+            vDiControllo += shuffledArray[nPrimoPacchetto];
 
-    }
-    vDiControllo=vDiControllo/a;
-    vStrategia1=vStrategia1/a;
-    vStrategia2=vStrategia2/a;
-    System.err.println(vStrategia1+","+vStrategia2+","+vAtteso+";"+vDiControllo);
+            // Selezione del secondo pacchetto (diverso dal primo)
+            int nSecondoPacchetto;
+            do {
+                nSecondoPacchetto = random.nextInt(10);
+            } while (nSecondoPacchetto == nPrimoPacchetto);
+
+            // Strategia basata sul valore del secondo pacchetto
+            if (shuffledArray[nSecondoPacchetto] > 5) {
+                // Strategia 1: Mantieni il primo pacchetto
+                vStrategia1 += shuffledArray[nPrimoPacchetto];
+
+                // Strategia 2: Cambia con un terzo pacchetto
+                int nTerzoPacchetto;
+                do {
+                    nTerzoPacchetto = random.nextInt(10);
+                } while (nTerzoPacchetto == nPrimoPacchetto || nTerzoPacchetto == nSecondoPacchetto);
+                vStrategia2 += shuffledArray[nTerzoPacchetto];
+            } else {
+                // Strategia 1: Cambia con un terzo pacchetto
+                int nTerzoPacchetto;
+                do {
+                    nTerzoPacchetto = random.nextInt(10);
+                } while (nTerzoPacchetto == nPrimoPacchetto || nTerzoPacchetto == nSecondoPacchetto);
+                vStrategia1 += shuffledArray[nTerzoPacchetto];
+
+                // Strategia 2: Mantieni il primo pacchetto
+                vStrategia2 += shuffledArray[nPrimoPacchetto];
+            }
+        }
+
+        // Calcolo delle medie
+        vDiControllo /= a;
+        vStrategia1 /= a;
+        vStrategia2 /= a;
+
+        // Stampa dei risultati
+        System.err.println("Strategia 1: " + vStrategia1);
+        System.err.println("Strategia 2: " + vStrategia2);
+        System.err.println("Valore atteso: " + vAtteso);
+        System.err.println("Controllo: " + vDiControllo);
     }
 }
+
